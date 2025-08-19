18b5b2b0196965062b0c4505ab9291b774d251f6
Vivek2122 <vpal21324@gmail.com>
2025-08-19 14:10:39 -0400

    remove /1

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://type-rush-backend.vercel.app/", {
+const socket = io("https://type-rush-backend.vercel.app", {
 	withCredentials: true,
 });
 
